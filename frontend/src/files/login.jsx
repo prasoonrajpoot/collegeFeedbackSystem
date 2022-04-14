d6faf3df0a00e76f279e5329317e4fe46d6711e0
Purvesh Doud <purveshdoud27@gmail.com>
2022-04-14 07:58:42 +0530

    Register and Login Structure added

--- frontend/src/files/login.jsx
+++ frontend/src/files/login.jsx
@@ -5,7 +5,8 @@ import axios from "axios";
 function Login(){
 
 
-
+    var [userName, setUserName] = React.useState("");
+    var [password, setPassword] = React.useState("");
 
     const sendData = async() => {
         
@@ -14,7 +15,14 @@ function Login(){
     console.log("we are here")
     return (
         <div>
-            this is Login route
+            <h1>Login</h1>
+            <form action="">
+                <label htmlFor="">Username</label><br />
+                <input type="text" onChange={(e) => setUserName(e.target.value)} /><br />
+                <label htmlFor="">Password</label><br />
+                <input type="text" onChange={(e) => setPassword(e.target.value)} /><br />
+                <input type="submit" />
+            </form>
         </div>
     )
 }
