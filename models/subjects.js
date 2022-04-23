ee5b5676bf0717ab7346dd3a0cb037488bab4704
Prasoon Rajpoot <betusingh890@gmail.com>
2022-04-23 18:34:50 +0530

    jlkdfj

--- models/subjects.js
+++ models/subjects.js
@@ -9,6 +9,19 @@ const subjectScheema = mongoose.Schema({
         required : true
     },
 
+    Branch : {
+        type : String,
+        required : true
+    },
+    Section : {
+        type : String,
+        required : true
+    },
+    Semester : {
+        type : Number,
+        required : true
+    },
+
     teacher : {
         required : true,
         type: mongoose.Schema.Types.ObjectId
