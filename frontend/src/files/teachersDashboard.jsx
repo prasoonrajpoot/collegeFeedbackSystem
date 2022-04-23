551152678a341e55fcdc76c6af98f05904273147
Deep Panchani <dlplearning00@gmail.com>
2022-04-23 20:17:13 +0530

    CSS for Admin and Teacher Dashboard

--- frontend/src/files/teachersDashboard.jsx
+++ frontend/src/files/teachersDashboard.jsx
@@ -4,21 +4,31 @@ import axios from "axios";
 
 function teachersDashboard(){
     return (
-        <div class="blue-box">
+        <div class="blue-box" style={{width:864}}>
             <div>
                 <span class="heading3">Deep Panchani </span>
                 <span class="heading3"><u>deeppanchani@iiitn.ac.in</u></span>
             </div>
-            <div class="blue-box">
-                <h1>Feedback Responses</h1>
-                <div>
-                    <div>
+            <div>
+                <h1 style={{margin:30}}>Feedback Responses</h1>
+                <div class="blue-box grid_container" style={{width:671, height:148, marginTop:"auto", marginBottom:20, padding:0}}>
+                    <div style={{margin:"auto", padding:"auto"}}>
                         <h3>4th Sem CSE Feedback Form</h3>
                         <h4>DAA Section B</h4>
                         <span>Results On: 18th may 2022</span>
                     </div>
-                    <div >
-                        <button class="form_button_blue">View Responses</button>
+                    <div style={{margin:"auto", padding:"auto"}}>
+                        <button class="form_button_blue" >View Responses</button>
+                    </div>
+                </div>
+                <div class="blue-box grid_container" style={{width:671, height:148, marginTop:"auto", marginBottom:20, padding:0}}>
+                    <div style={{margin:"auto", padding:"auto"}}>
+                        <h3>4th Sem CSE Feedback Form</h3>
+                        <h4>DAA Section B</h4>
+                        <span>Results On: 18th may 2022</span>
+                    </div>
+                    <div style={{margin:"auto", padding:"auto"}}>
+                        <button class="form_button_blue" >View Responses</button>
                     </div>
                 </div>
             </div>
