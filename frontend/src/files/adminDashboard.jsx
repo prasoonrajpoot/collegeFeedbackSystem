7c51b8e0122cb7e82e70481904d6301c9f8cb596
Krushna Patil <patilkrushna1208@gmail.com>
2022-04-23 10:48:55 +0530

    dashboard updated

--- frontend/src/files/adminDashboard.jsx
+++ frontend/src/files/adminDashboard.jsx
@@ -4,20 +4,10 @@ import axios from "axios";
 
 function adminDashboard(){
     return (
-        <div>
+        <div class="blue-box">
             <h1>Feedback Forms</h1>
-            <div>
+            <div class="blue-box">
                 <h3>4th Sem Feedback Form</h3>
-                <h4>Deadline: 18th May 2022</h4>
-                <button><span>View Responses</span></button>
-            </div>
-            <div>
-                <h3>5th Sem Feedback Form</h3>
-                <h4>Deadline: 18th May 2022</h4>
-                <button><span>View Responses</span></button>
-            </div>
-            <div>
-                <h3>6th Sem Feedback Form</h3>
                 <h4>Deadline: 18th May 2022</h4>
                 <button><span>View Responses</span></button>
             </div>
