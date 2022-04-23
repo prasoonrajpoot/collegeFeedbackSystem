10220d9094ba2c0b0aac481d74833ed9c2dbd120
Prasoon Rajpoot <betusingh890@gmail.com>
2022-04-23 12:05:05 +0530

    adfads

--- frontend/src/files/adminDashboard.jsx
+++ frontend/src/files/adminDashboard.jsx
@@ -2,15 +2,28 @@ import React from "react";
 import axios from "axios";
 
 
+
+
 function adminDashboard(){
+
+    var [TeacherName, setTeacherName] = React.useState("")
+    
+
+
+    AddTeacherPressed = async (event) => {
+        event.preventDefault();
+        var obj = {Name : TeacherName};
+        axios.post("/addteacher", obj);
+    }
+
     return (
         <div class="blue-box">
-            <h1>Feedback Forms</h1>
-            <div class="blue-box">
-                <h3>4th Sem Feedback Form</h3>
-                <h4>Deadline: 18th May 2022</h4>
-                <button><span>View Responses</span></button>
-            </div>
+            Add teacher
+            <form action="">
+                <input type="text" placeholder = "Teacher's Name" value = {TeacherName} 
+                onChange = {(e) => setTeacherName(e.target.value)}/>
+                <button >Add Teacher</button>
+            </form>
         </div>
     )
 }
